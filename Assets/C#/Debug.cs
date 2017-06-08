9c1e220ead86a5ad0ec3da7f6ff9ef2c4e82fdab
DESKTOP-KO2DP2J\KICHUL <kichul1122@gmail.com>
2017-06-08 18:46:44 +0900

    06-08

---
+++ Assets/C#/Debug.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DebugCustom
+{
+	public enum Color { Red, Green, Max }
+	public static void Log(string log, Color color)
+	{
+		UnityEngine.Debug.Log(string.Format("<color={0}>{1}</color>", color.ToString().ToLower(), log));
+	}
+	
+}
---
+++ Assets/C#/Utility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utility
+{
+	public static string NowTime()
+	{
+		//return System.DateTime.Now.ToString("yy/MM/dd-HH:mm:ss");
+		return System.DateTime.Now.ToShortTimeString();
+	}
+
+}
