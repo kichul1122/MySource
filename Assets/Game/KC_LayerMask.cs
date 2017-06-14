5405911d5ac58ab113837307fd7f5922d0f3a720
DESKTOP-KO2DP2J\KICHUL <kichul1122@gmail.com>
2017-06-14 21:41:07 +0900

    06-14

---
+++ Assets/Game/KC_LayerMask.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KC_LayerMask : MonoBehaviour
+{
+	public LayerMask layerMask = -1;	//1111 1111 1111 1111 1111 1111 1111 1111
+
+	private void Start()
+	{
+		//layerMask = LayerMask.GetMask("Player", "Enemy");
+		//layerMask = 1 << 9 | 1 << 10;
+		//layerMask = 1 << LayerMask.NameToLayer("Player") | 1 << LayerMask.NameToLayer("Enemy");
+
+		//Player 9, Enemy 10
+		Debug.Log(LayerMask.GetMask("Player", "Enemy"));
+		//result : 2^9 + 2^10 = 1536
+
+		Debug.Log(LayerMask.NameToLayer("Player"));//bit index
+		Debug.Log(LayerMask.NameToLayer("Enemy"));//bit index
+
+		Debug.Log(LayerMask.LayerToName(9));//bit index
+		Debug.Log(LayerMask.LayerToName(10));//bit index
+	}
+}
---
+++ Assets/Game/KC_Raycast.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KC_Raycast : MonoBehaviour
+{
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+		Show2D();
+	}
+
+	#region 2D
+	[Header("2D")]
+	public Vector2 origin2D;
+	public Vector2 direction2D;
+	public float distance;
+	public LayerMask layerMask;
+
+	public void Show2D()
+	{
+		direction2D.Normalize();
+		RaycastHit2D raycastHit2D;
+		//raycastHit2D = Physics2D.Raycast(origin2D, direction2D);
+		//Debug.DrawLine(origin2D, origin2D + direction2D * Mathf.Infinity);
+
+		//raycastHit2D = Physics2D.Raycast(origin2D, direction2D, distance);
+		//Debug.DrawLine(origin2D, origin2D + direction2D * distance);
+
+		//layerMask = LayerMask.GetMask("Player", "Enemy");
+		//layerMask = 1 << 9 | 1 << 10;
+		//layerMask = 1 << LayerMask.NameToLayer("Player") | 1 << LayerMask.NameToLayer("Enemy");
+		raycastHit2D = Physics2D.Raycast(origin2D, direction2D, distance, layerMask);
+		Debug.DrawLine(origin2D, origin2D + direction2D * distance);
+
+		if (raycastHit2D)
+		{
+			Debug.Log("raycastHit2D");
+		}
+	}
+	#endregion
+
+	[Header("3D")]
+	public Vector3 origin3D;
+	public Vector3 direction3D;
+	public void Show3D()
+	{
+		//Physics.Raycast
+	}
+}
