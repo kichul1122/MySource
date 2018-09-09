143281360ee67db52d97954da18eae3e7afd83ab
DESKTOP-KO2DP2J\KICHUL <kichul1122@gmail.com>
2018-09-09 14:46:10 +0900

    DisallowMultiple

---
+++ Assets/Architecture/DisallowMultiple.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[DisallowMultipleComponent]
+public abstract class ATest1 : SerializedMonoBehaviour
+{
+	public abstract void TestA();
+}
+
+public class DisallowMultiple : ATest1 
+{
+	[Button]
+	public override void TestA()
+	{
+		Debug.Log("Wow DisallowMultiple class");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
